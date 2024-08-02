a598bbf985eb4a6535135a421cd90e01e85ffc39
HulitosCode <heltonmatimbe8@gmail.com>
2024-08-02 09:21:30 +0200

    Atualizando o site todo

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -21,7 +21,9 @@ const Cards = () => {
                         <p className='py-2 border-b mx-8'>Requisitos de Implantação</p>
                         <p className='py-2 border-b mx-8'>Requisitos de Manutenção e Suporte</p>
                     </div>
-                    <button className='bg-blue-600 text-white w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Contactar</button>
+                    <button className='bg-blue-600 text-white w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>
+                        <a href="https://wa.me/+258833064723" target="_blank" rel="noopener noreferrer">Whatsapp</a>
+                    </button>
                 </div>
                 <div className='w-full shadow-2xl border-2 border-purple-600 flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300'>
                     <h2 className='text-2xl font-bold text-center py-8'>Criação de sites</h2>
@@ -36,11 +38,13 @@ const Cards = () => {
                         <p className='py-2 border-b mx-8'>Identidade Visual Completa</p>
                         <p className='py-2 border-b mx-8'>Consultoria de Marketing Digital</p>
                         <p className='py-2 border-b mx-8'>SEO Avançado</p>
-                        <p className='py-2 border-b mx-8'>Manutenção e Atualizações por 1 Ano</p>
+                        <p className='py-2 border-b mx-8'>Manutenção e Atualizações por 3 Meses</p>
                         <p className='py-2 border-b mx-8'>1 Ano de Dominio Gratuito</p>
                         <p className='py-2 border-b mx-8'>1 Ano de Hospedagem Gratuita</p>
                     </div>
-                    <button className='bg-purple-600 text-white w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Contactar</button>
+                    <button className='bg-purple-600 text-white w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>
+                        <a href="https://wa.me/+258833064723" target="_blank" rel="noopener noreferrer">Whatsapp</a>
+                    </button>
                 </div>
                 <div className='w-full border-2 border-pink-600 shadow-2xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
                     <h2 className='text-2xl font-bold text-center py-8'>Identidade Visual</h2>
@@ -58,7 +62,9 @@ const Cards = () => {
                         <p className='py-2 border-b mx-8'>Brindes</p>
                         <p className='py-2 border-b mx-8'>Banners e Faixas</p>
                     </div>
-                    <button className='bg-pink-600 w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3 text-white'>Contactar</button>
+                    <button className='bg-pink-600 w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3 text-white'>
+                        <a href="https://wa.me/+258833064723" target="_blank" rel="noopener noreferrer">Whatsapp</a>
+                    </button>
                 </div>
             </div>
         </div>
